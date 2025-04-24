c724eea35f9b9a2bc88a8b8b4f43b4f7869be358
JuanLamas <juanedulam2@gmail.com>
2025-04-24 16:53:52 -0300

    Se agrega toda la estructura del proyecto

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import App2 from './assets/components/App2.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
