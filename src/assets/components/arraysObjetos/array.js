60a7994b35f927622cc154b0bcf8373e13bd0b68
pricila acsama <pricilaacsama12@gmail.com>
2025-04-29 19:28:11 -0300

    Pricila Acsama - subiendo la parte logica de Productos

--- src/assets/components/arraysObjetos/array.js
+++
@@ -1,8 +0,0 @@
-export const productos = [
-    {descripcion: "Teclado", precio: 34000},
-    {descripcion: "Mouse", precio: 23000},
-    {descripcion: "Parlantes", precio: 38500},
-    {descripcion: "Router", precio: 19000},
-    {descripcion: "Cable HDMI", precio: 9500},
-    {descripcion: "WebCam", precio: 14000}
-];
