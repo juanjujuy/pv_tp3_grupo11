95f74349475565837fdf84c20ac6f035c6451ad6
JuanLamas <juanedulam2@gmail.com>
2025-04-30 00:33:44 -0300

    Mejorar detalles de TaskInput. Juan Lamas

--- src/assets/components/TaskInput.jsx
+++ src/assets/components/TaskInput.jsx
@@ -16,17 +16,19 @@ const agregarTarea = (event) => {
     event.preventDefault();
     setLista([...lista, nuevo]);
     console.log(nuevo);
+    setNuevo("");
 
 }
 
     return(
         <div className="task-input-container">
-            <h3>Nueva Tarea</h3>
+            <h2>Nueva Tarea</h2>
             <form onSubmit={agregarTarea}>
             <label>Ingrese la nueva Tarea</label>
             <input type="text" value={nuevo} onChange={guardarNombre}/>
             <button type="submit">Agregar</button>
             </form>
+            <h2>Ultima Tarea Agregada</h2>
             <TaskItem tarea={lista[lista.length-1]}></TaskItem>
         </div>
     );
